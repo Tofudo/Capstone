87ae4bd9040d6aa1fa9ffa5fbb3e7fc7f2cd0e16
sophiaacohon <sophiaannu.cohon@gmail.com>
2025-10-12 17:50:33 +0800

    adjusted login page, made use of the login.scss file

--- resources/views/authorized/login.blade.php
+++ resources/views/authorized/login.blade.php
@@ -1,22 +1,17 @@
 @extends('layouts.app')
 @section('content')
-    <section class="class= vh-100 d-flex align-items-center justify-content-center"
-    style="background-image: url('{{ asset('images/login-bg.png') }}');
-           background-size: cover;
-           background-position: center;
-           background-repeat: no-repeat;
-           margin: 0;
-           padding: 0;">
+    <section class="vh-100 d-flex align-items-center justify-content-center login-bg"
+    style="background-image: url('{{ asset('images/login-bg.png') }}');">
         <div class="container py-5 h-100">
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-                    <div class="card" style="border-radius: 1rem; background-color: #ffffff; color: #485B8C;">
+                    <div class="card">
                         <div class="card-body py-0 px-5 text-center">
 
                             <div class="mb-md-4 mt-md-4 py-0">
-                            <img src="{{ asset('images/lslc_logo_name2.png') }}" alt="App Logo" class="img-fluid mb-3" style="height:150px">
-                                <h2 class="fw-bold mb-1 mt-1 text-uppercase" style="color: #485B8C;">WELCOME</h2>
-                                <p class="mb-5" style="color: #485B8C;">Enter your username and password</p>
+                            <img src="{{ asset('images/lslc_logo_name2.png') }}" alt="App Logo" class="img-fluid mb-1" style="height:150px">
+                                <h2 class="fw-bold text-uppercase">WELCOME</h2>
+                                <p class="mb-3">Enter your username and password</p>
 
 
                                 {{-- Display errors --}}
@@ -34,7 +29,6 @@
                                     <div class="form-floating mb-4">
                                         <input type="text" name="username" id="typeEmailX"
                                             class="form-control form-control-lg"
-                                            style="background-color: #ffffff; color: #485B8C; border: 1px solid #485B8C;"
                                             placeholder="Username" value="{{ old('username') }}" />
                                         <label for="typeEmailX" class="#485B8C">Username</label>
                                     </div>
@@ -42,14 +36,12 @@
                                     <!-- Password -->
                                     <div class="form-floating mb-5">
                                         <input type="password" name="password" id="typePasswordX"class="form-control form-control-lg"
-                                            style="background-color: #ffffff; color: #485B8C; border: 1px solid #485B8C;"
                                             class="form-control form-control-lg text-white bg-dark border-light"
                                             placeholder="Password" />
                                         <label for="typePasswordX" class="#485B8C">Password</label>
                                     </div>
 
-                                    <button class="btn btn-lg px-5 mt-3 mb-4"
-                                            style="background-color: #485B8C; color: white; border: none;"
+                                    <button class="loginBtn btn btn-lg px-5 mt-1 mb-4"
                                             type="submit">
                                         Login
                                     </button>
